0b26ea5e141dfb8bb66faa2dcdca7f30d357ad9a
Islash8 <islamali2007@gmail.com>
2025-06-05 04:34:43 +0200

    adding new configuration to hyprland, fish and kitty

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    fastfetch
+    alias cdwm="nvim ~/Builds/dwm-btw/config.h"
+    alias e="nvim"
+    alias mdwm="cd ~/Builds/dwm-btw/; sudo make clean install; cd -"
+    alias ehypr="nvim ~/.config/hypr/hyprland.conf"
 end
